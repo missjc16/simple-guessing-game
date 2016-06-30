34ecca2725dfd8f7d20f9e51a1b6ecb91d0413f6
saimonh3 <sajjad2721@gmail.com>
2016-06-30 12:37:56 +0600

    first version

---
+++ guessing-game/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Guessing Game</title>
+</head>
+<body>
+    <style type="text/css">
+        .container{
+            width: 1200px;
+            text-align: center;
+        }
+    </style>
+    <div class="container">
+        <div class="logo">
+            <h1>Gessing Game</h1>
+        </div>
+        <form action="" method="POST">
+            <input type="text" name="text">
+            <input type="submit" name="submit" value="Guess It">
+        </form>
+    </div>
+    <?php
+    $number = rand(1,10);
+        if( isset($_POST['submit']) ){
+            $text = $_POST['text'];
+            if(empty($text)){ ?>
+                <div class="container">
+                    <h2><?php echo 'Please guess any number between 1 to 10' ?></h2>
+                </div>
+            <?php }else{
+                if($text == $number){ ?>
+                   <div class="container">
+                       <h2><?php echo 'Hurrah..! You won the game.'; ?></h2>
+                   </div>
+
+               <?php }else{ ?>
+                   <div class="container">
+                       <h2><?php echo 'Sorry...! Best luck for next time.'; ?></h2>
+                   </div>
+
+                <?php }
+            }
+        }
+
+    ?>
+
+</body>
+</html>
